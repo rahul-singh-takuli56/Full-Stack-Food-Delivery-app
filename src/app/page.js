2e8ca7e8b5f55c3d1f99200308a1d673363f55f8
Rahul Singh Takuli <118590603+rahul-singh-takuli56@users.noreply.github.com>
2024-03-11 13:43:15 +0530

    fixed eslint error

--- src/app/page.js
+++ src/app/page.js
@@ -15,11 +15,11 @@ export default function Home() {
           mainHeader={'About Us'}
         />
         <div className="text-gray-500 max-w-2xl mx-auto mt-4 flex flex-col gap-4">
-          <p>At <span className="text-orange-600 font-semibold">RST PIZZA,</span> we're not just about pizza; we're about creating experiences, fostering connections, and satisfying cravings. Founded on a passion for great food and friendly service, our journey began with a simple vision: to serve mouthwatering pizzas made with the finest ingredients and crafted with love.
+          <p>At <span className="text-orange-600 font-semibold">RST PIZZA,</span> we&apos;re not just about pizza; we&apos;re about creating experiences, fostering connections, and satisfying cravings. Founded on a passion for great food and friendly service, our journey began with a simple vision: to serve mouthwatering pizzas made with the finest ingredients and crafted with love.
           </p>
-          <p >What sets us apart is our commitment to quality. From handcrafted dough prepared fresh daily to locally sourced toppings bursting with flavor, every bite of our pizza is a testament to our dedication to excellence. But it's not just about what goes into our pizzas, it's also about the care and attention that goes into every order.
+          <p >What sets us apart is our commitment to quality. From handcrafted dough prepared fresh daily to locally sourced toppings bursting with flavor, every bite of our pizza is a testament to our dedication to excellence. But it&apos;s not just about what goes into our pizzas, it&apos;s also about the care and attention that goes into every order.
           </p>
-          <p >Beyond our delicious pizzas, we're a community hub, a place where friends and families gather to share laughter, stories, and of course, slices. Our warm and inviting atmosphere invites you to sit back, relax, and enjoy the simple pleasure of good food and good company.
+          <p >Beyond our delicious pizzas, we&apos;re a community hub, a place where friends and families gather to share laughter, stories, and of course, slices. Our warm and inviting atmosphere invites you to sit back, relax, and enjoy the simple pleasure of good food and good company.
           </p>
         </div>
       </section>
--- src/components/Menu/MenuItem.js
+++ src/components/Menu/MenuItem.js
@@ -1,14 +1,16 @@
+import Image from 'next/image'
 import React from 'react'
 
 const MenuItem = () => {
     return (
         <div className='bg-gray-200 p-4 rounded-lg text-center group hover:bg-white transition-all hover:shadow-md hover:shadow-black/25 '>
-            <div className='text-center  '>
-                <img src="/pizza.png" alt="pizza" className='max-h-52  block mx-auto ' />
+            <div className='text-center mx-auto'>
+                <Image src={'/pizza.png'} alt="pizza" width={300} height={100} />
+
             </div>
             <h4 className='font-bold my-3 text-xl'>Pepperoni Pizza</h4>
-            <p className='text-gray-500 text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit fjdslfjsdlfjsdf l jsdlf sd fsd fjsdlf sdf</p>
-
+            <p className='text-gray-500 text-sm'>Experience the pinnacle of pizza craftsmanship, Embark on a flavor-filled journey with our exquisite pizzas.
+            </p>
             <button className='bg-primary mt-4 text-white rounded-full px-8 py-2'>Add to Cart $20</button>
 
         </div>
